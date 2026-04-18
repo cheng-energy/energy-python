5618d3a5b9e0372d6abe6d2df4a2f45d6a82625b
chx <xiaohuanshuijiao@qq.com>
2026-04-18 08:53:36 +0800

    关于网页解析的部分代码，包括把请求到的数据写入.html文本，然后通过fromstring方法，把html中的文本转化成文本文件

--- 03-projects/AI实战/网络机器人（爬虫）.py
+++ 03-projects/AI实战/网络机器人（爬虫）.py
@@ -1,9 +1,16 @@
 import requests
+from lxml import html
+
 # 定义URL
 url = 'https://www.tiobe.com/tiobe-index/'
 
 # 使用requests库来发送HTTP请求
 response = requests.get(url)
 
-# 输出结果到控制台
-print(response.text)  # 返回的是前端页面代码
+
+with open('爬虫.html','w',encoding='utf8') as f:
+    f. write(response.text)
+    # 把html文件的文本转化成文本文件
+    data = html.fromstring('爬虫.html')
+    # 使用Xpath对内容进行解析
+    
