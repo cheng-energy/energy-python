5c93ecfe30ae9ea857b0a55d8147675f45657d51
chx <xiaohuanshuijiao@qq.com>
2026-04-18 10:03:40 +0800

    爱

--- 03-projects/AI实战/网络机器人（爬虫）.py
+++ 03-projects/AI实战/网络机器人（爬虫）.py
@@ -10,7 +10,13 @@ response = requests.get(url)
 
 with open('爬虫.html','w',encoding='utf8') as f:
     f. write(response.text)
-    # 把html文件的文本转化成文本文件
-    data = html.fromstring('爬虫.html')
-    # 使用Xpath对内容进行解析
-    
+# 把html文件的文本转化成文本对象，后续可以使用xpath语法
+data = html.fromstring(response.text)
+# 使用Xpath对内容进行解析
+head = data.xpath('//*[@id="top20"]/thead/tr/th/text()')
+print(head)
+#获取表格的其余数据
+body_tr = data.xpath('//*[@id="top20"]/tbody/tr')
+for tr in body_tr:
+    data_td = tr.xpath('./td/text()')
+    print(data_td)
