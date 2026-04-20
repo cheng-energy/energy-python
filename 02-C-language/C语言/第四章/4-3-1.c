54c64c9ce7ae989c5e9927e0977d14d4b58a4ab9
chx <xiaohuanshuijiao@qq.com>
2026-04-20 20:32:20 +0800

    C语言作业的提交

--- 02-C-language/C语言/第四章/4-3-1.c
+++ 02-C-language/C语言/第四章/4-3-1.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
-int main() {
-    printf("Hello\n");
-    return 0;
+#include <math.h>
+#define PI 3.1415926
+int main()
+{
+    double y,x;
+    printf("请输入x的值:");
+    scanf("%lf",&x);
+    if(x < PI / 2)
+    {
+        y = 2 * pow(x,3) + 3*cos(x) +5;
+        
+    }
+    else if (x >= -PI/2 && x <= PI/2)
+    {
+        y = pow((x-1)/(x+2),3) + 5 * x;
+      
+    }
+    else
+    {
+        y = pow(x+2*sin(3*x),0.5);
+      
+    }
+    printf("y = %.4f\n",y);
+    return 0 ;
 }
-
-
-
-
-
 
 
 
